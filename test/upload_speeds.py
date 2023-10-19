bb7a8518cdbad47464ca21012cec3d813c4890e1
nickeisenberg <nickeisenberg@gmail.com>
2023-10-18 22:28:56 -0700

    trying to get the progress bar to work

--- test/upload_speeds.py
+++ test/upload_speeds.py
@@ -28,10 +28,8 @@ img_t = transform(img)
 #--------------------------------------------------
 with open("/home/nicholas/.credentials/password.json") as oj:
     pw = json.load(oj)
-
 ACCESS_KEY = pw['aws_ACCESS_KEY_nick']
 SECRET_ACCESS_KEY = pw['aws_SECRET_ACCESS_KEY_nick']
-
 session = boto3.Session(
     aws_access_key_id=ACCESS_KEY,
     aws_secret_access_key=SECRET_ACCESS_KEY,
@@ -39,7 +37,6 @@ session = boto3.Session(
     region_name="us-west-1"
 )
 #--------------------------------------------------
-
 
 #--------------------------------------------------
 # delete and remake the folder for testing
