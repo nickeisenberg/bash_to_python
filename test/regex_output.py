3693e208b2de8282c5746cd1383f15de789ed7db
nickeisenberg <nickeisenberg@gmail.com>
2023-10-20 13:06:42 -0700

    reorganize

--- test/regex_output.py
+++
@@ -1,8 +0,0 @@
-import re
-
-string = "Completed 65.0 MiB/65.9 MiB (4.4 MiB/s) with 5 file(s) remaining"
-
-l = string.split(" ")[:6]
-
-"PROGRESS " + str.join(" ", l[1:4]) + "    SPEED " + str.join(" ", l[4:])
-
