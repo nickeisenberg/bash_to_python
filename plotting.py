5e230d6934a0cf2c69c525fda4e31ffef5986626
nickeisenberg <nickeisenberg@gmail.com>
2023-10-20 17:25:27 -0700

    added the wring plotting scrpt. replaced with the correct

--- plotting.py
+++ plotting.py
@@ -1,25 +1,26 @@
 import matplotlib.pyplot as plt
 import mpld3
 from flask import Flask
+import matplotlib.pyplot as plt
 
-app = Flask(__name__)
+def ec2plotter(
+    fig,
+    public_ip
+    ):
 
-# Function to generate a Matplotlib plot
-def generate_plot():
-    fig, ax = plt.subplots()
-    ax.plot([1, 2, 3, 4], [1, 4, 9, 16])
-    ax.set_xlabel('X-axis')
-    ax.set_ylabel('Y-axis')
-    ax.set_title('Matplotlib Plot')
-    return fig
+    app = Flask(__name__)
 
-@app.route('/')
-def plot_page():
-    fig = generate_plot()
-    # Convert the Matplotlib figure to an interactive D3.js plot
-    interactive_plot = mpld3.fig_to_html(fig)
-    return interactive_plot
+    @app.route('/')
+    def _plot(fig=fig):
+        interactive_plot = mpld3.fig_to_html(fig)
+        return interactive_plot
 
-if __name__ == '__main__':
-    app.run(host="192.168.1.32")
+    _plot(fig=fig)
+    app.run(host=public_ip)
 
+    return None
+
+
+fig = plt.figure()
+plt.plot([1, 2, 3], [1, 2, 3])
+plt.xlabel("x axis")
