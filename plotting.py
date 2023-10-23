5e053fa677ce5a74b676f7d019f81b743f680b14
nickeisenberg <nickeisenberg@gmail.com>
2023-10-23 07:28:03 -0700

    plotting will not work it appears

--- plotting.py
+++ plotting.py
@@ -7,20 +7,18 @@ def ec2plotter(
     fig,
     public_ip
     ):
-
     app = Flask(__name__)
-
     @app.route('/')
     def _plot(fig=fig):
         interactive_plot = mpld3.fig_to_html(fig)
         return interactive_plot
-
     _plot(fig=fig)
-    app.run(host=public_ip)
-
+    app.run(host=public_ip, port=80)
     return None
 
 
 fig = plt.figure()
 plt.plot([1, 2, 3], [1, 2, 3])
 plt.xlabel("x axis")
+
+ec2plotter(fig, "0.0.0.0")
