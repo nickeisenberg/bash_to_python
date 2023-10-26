12da881f7e30f6a47067153216a5cb1f0172f509
nickeisenberg <nickeisenberg@gmail.com>
2023-10-25 17:24:41 -0700

    trigger update

--- _test/plotting/trigger_workflow/trigger.py
+++ _test/plotting/trigger_workflow/trigger.py
@@ -3,17 +3,18 @@
 #--------------------------------------------------
 
 import matplotlib.pyplot as plt
-from plotting import Plotter
+from pyaws.plotting import Plotter
 
 port = "2222"
 save_path = "/home/nicholas/Tmp/png_trigger_folder"
 user = "nicholas"
 ip = "174.72.155.21"
-path_to_bash = "/home/ubuntu/gitrepos/pyaws/transfer/scripts/scp.sh"
+# path_to_bash = "/home/ubuntu/gitrepos/pyaws/transfer/scripts/scp.sh"
 
-plotter = Plotter(user, ip, save_path, port, path_to_bash=path_to_bash)
+# plotter = Plotter(user, ip, save_path, port, path_to_bash=path_to_bash)
+plotter = Plotter(user, ip, save_path, port)
 
 fig = plt.figure()
-plt.plot([1, 2, 3, 4, 5], [1, 3, 2, 4, 9])
+plt.plot([1, 2, 3, 4, 5], [1, 3, 2, 4, 8])
 
-plotter.show("test2", fig)
+plotter.show("test1", fig)
