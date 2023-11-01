0f7d145e356142d895814f9122d4909a703ced4a
nickeisenberg <nickeisenberg@gmail.com>
2023-11-01 11:27:36 -0700

    the tree depth bug seems to be fixed

--- _test/pytorch_utils/dataset_test.py
+++ _test/pytorch_utils/dataset_test.py
@@ -1,22 +1,14 @@
 from pyaws.pytorch_utils import TreeImageFolder
 import os
 
-
 HOME = os.environ['HOME']
-
-# this is buggy becuase of how I am taking care of depth. need to fix
 root = f"{HOME}/GitRepos/pyaws_project/pyaws/_test/pytorch_utils/data"
 
-# this will work
-root = "data"
+dataset = TreeImageFolder(root=root, depth_limit=2)
 
-dataset = TreeImageFolder(root=root, depth_limit=1)
+print(dataset.fullpaths)
 
-dataset.fullpaths
+print(dataset.basepaths)
 
 for inp in dataset.dataset:
     print(inp)
-
-os.path.basename(inp[0])
-
-os.path.basename("data")
