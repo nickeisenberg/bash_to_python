0998bb3d25479269dc84489d107eaeb51351a66c
nickeisenberg <nickeisenberg@gmail.com>
2023-10-31 21:55:28 -0700

    id a bug with the dataloader. need tp change how I calculate depth in the treeimagefolder class

--- _test/pytorch_utils/dataset_test.py
+++ _test/pytorch_utils/dataset_test.py
@@ -1,6 +1,11 @@
 from pyaws.pytorch_utils import TreeImageFolder
 
+# this is buggy becuase of how I am taking care of depth. need to fix
+root = "/home/nicholas/GitRepos/pyaws_project/pyaws/_test/pytorch_utils/data"
+
+# this will work
 root = "data"
+
 dataset = TreeImageFolder(root=root, depth_limit=1)
 for inp in dataset.dataset:
     print(inp)
