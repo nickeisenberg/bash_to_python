6d545938712fc8c27c6274877dc0f3b93d0d4503
Nick Eisenberg <nickeisenberg@gmail.com>
2023-11-01 10:38:31 -0700

    dpeth correction to dataloader

--- pytorch_utils/treeimagefolder.py
+++ pytorch_utils/treeimagefolder.py
@@ -11,25 +11,31 @@ class TreeImageFolder:
         self.dataset = []
         self.get_all_classes(root, depth_limit)
         self.make_dataset(depth_limit)
+        self.depth_corrector = self.split_path(root) - 1
 
 
     def get_all_classes(self, root, depth_limit=1):
     
-        dirs = [d.name for d in os.scandir(root) if os.path.isdir(os.path.join(root, d.name))]
-    
+        dirs = [
+            d.name for d in os.scandir(root) 
+            if os.path.isdir(os.path.join(root, d.name))
+        ]
+
         for dir in dirs:
-    
+
             target_dir = os.path.join(root, dir)
-    
+
             innerdirs = [
-                d.name for d in os.scandir(target_dir) if os.path.isdir(os.path.join(target_dir, d.name))
+                d.name for d in os.scandir(target_dir) 
+                if os.path.isdir(os.path.join(target_dir, d.name))
             ]
-    
+
             if not innerdirs:
                 if target_dir not in self.classes:
                     self.classes.append(target_dir)
             else:
-                curdepth = len(target_dir.split('/')) - 1
+                # curdepth = len(target_dir.split('/')) - 1
+                curdepth = self.split_path(target_dir) - self.depth_corrector
                 if curdepth == depth_limit:
                     self.classes.append(target_dir)
                     continue
@@ -54,5 +60,25 @@ class TreeImageFolder:
                         )
 
         return None
+
+    
+    @staticmethod
+    def split_path(path):
+        # Initialize an empty list to store path components
+        parts = []
+        
+        # Keep splitting the path until no more directories are left
+        while True:
+            path, folder = os.path.split(path)
+            if folder:
+                # Append the folder to the start of the parts list
+                parts.insert(0, folder)
+            else:
+                if path:
+                    # Append the drive (if any) to the start of the parts list
+                    parts.insert(0, path)
+                break
+    
+        return len(parts)
 
 
